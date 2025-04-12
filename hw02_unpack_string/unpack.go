6a4b9cda216172d52f69fc882bca0167a959fed4
Valentin Filshin <filshin.va@nikamed-it.ru>
2025-04-13 01:31:26 +0300

    HW2: Базовое решение задачи

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	counter := 0
+	result := make([]rune, 0, 8)
+	lastRune := rune(0)
+	stringLen := utf8.RuneCountInString(str)
+
+	for _, r := range str {
+		if counter == 0 && unicode.IsDigit(r) {
+			return "", ErrInvalidString
+		}
+		if counter > 0 {
+			if unicode.IsDigit(r) && unicode.IsDigit(lastRune) {
+				return "", ErrInvalidString
+			}
+
+			if unicode.IsDigit(r) {
+				digit := int(r - '0')
+				for i := 0; i < digit; i++ {
+					result = append(result, lastRune)
+				}
+			}
+
+			if !unicode.IsDigit(r) && !unicode.IsDigit(lastRune) {
+				result = append(result, lastRune)
+			}
+		}
+
+		counter++
+		lastRune = r
+
+		if counter == stringLen && !unicode.IsDigit(lastRune) {
+			result = append(result, lastRune)
+		}
+	}
+
+	return string(result), nil
 }
