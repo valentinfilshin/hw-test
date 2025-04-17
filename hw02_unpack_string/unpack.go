b4de9d9ec450ae16214c7127f2a5ac938e4425c7
Valentin Filshin <filshin.va@nikamed-it.ru>
2025-04-17 17:53:01 +0300

    HW2: оптимизация условий

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,39 +9,49 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	counter := 0
 	stringLen := utf8.RuneCountInString(str)
+
+	if stringLen == 0 {
+		return "", nil
+	}
+
+	firstRune, _ := utf8.DecodeRuneInString(str)
+	if unicode.IsDigit(firstRune) {
+		return "", ErrInvalidString
+	}
+
 	result := make([]rune, 0, stringLen)
 	lastRune := rune(0)
+	counter := 0
 
 	for _, r := range str {
-		if counter == 0 && unicode.IsDigit(r) {
+		if counter == 0 {
+			counter++
+			lastRune = r
+			continue
+		}
+
+		if unicode.IsDigit(r) && unicode.IsDigit(lastRune) {
 			return "", ErrInvalidString
 		}
 
-		if counter > 0 {
-			if unicode.IsDigit(r) && unicode.IsDigit(lastRune) {
-				return "", ErrInvalidString
-			}
-
-			if unicode.IsDigit(r) {
-				digit := int(r - '0')
-				for i := 0; i < digit; i++ {
-					result = append(result, lastRune)
-				}
-			}
-
-			if !unicode.IsDigit(r) && !unicode.IsDigit(lastRune) {
+		if unicode.IsDigit(r) {
+			digit := int(r - '0')
+			for i := 0; i < digit; i++ {
 				result = append(result, lastRune)
 			}
 		}
 
-		counter++
-		lastRune = r
-
-		if counter == stringLen && !unicode.IsDigit(lastRune) {
+		if !unicode.IsDigit(r) && !unicode.IsDigit(lastRune) {
 			result = append(result, lastRune)
 		}
+
+		counter++
+		lastRune = r
+	}
+
+	if counter == stringLen && !unicode.IsDigit(lastRune) {
+		result = append(result, lastRune)
 	}
 
 	return string(result), nil
