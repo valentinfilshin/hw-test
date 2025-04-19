a6e1751ab0c8439834d8a915c15a438390d73648
Valentin Filshin <filshin.va@nikamed-it.ru>
2025-04-19 09:59:22 +0300

    HW3: Решение задачи по сортировке слой в строке

--- hw03_frequency_analysis/go.mod
+++ hw03_frequency_analysis/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw03_frequency_analysis
+module github.com/valentinfilshin/hw-test/hw03_frequency_analysis
 
 go 1.23
 
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type Word struct {
+	Word string
+	Freq int
+}
+
+func Top10(text string) []string {
+	s := strings.Fields(text)
+	uniqueWords := make(map[string]int)
+
+	for _, v := range s {
+		uniqueWords[v]++
+	}
+
+	var words []Word
+	for k, v := range uniqueWords {
+		words = append(words, Word{k, v})
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].Freq == words[j].Freq {
+			return words[i].Word < words[j].Word
+		}
+		return words[i].Freq > words[j].Freq
+	})
+
+	result := make([]string, 0, 10)
+
+	for _, v := range words {
+		if len(result) == 10 {
+			break
+		}
+		result = append(result, v.Word)
+	}
+
+	return result
 }
