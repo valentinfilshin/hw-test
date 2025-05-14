307c8580574cf9521bcd2a572b7c284d7b6ed1b8
Valentin Filshin <filshin.va@nikamed-it.ru>
2025-05-14 17:44:43 +0300

    HW7: Базовое решение с багом в тесте

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,15 +1,86 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrCreateFile            = errors.New("can't create file")
+	ErrReadFile              = errors.New("can't read file")
+	ErrFileInfoNil           = errors.New("file info is nil")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if fromFileInfo == nil {
+		return ErrFileInfoNil
+	}
+
+	if !fromFileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fromFileSize := fromFileInfo.Size()
+
+	if offset > fromFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || limit > fromFileSize-offset {
+		limit = fromFileSize - offset
+	}
+
+	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer fromFile.Close()
+
+	if offset > 0 {
+		if _, err := fromFile.Seek(offset, io.SeekStart); err != nil {
+			return err
+		}
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return ErrCreateFile
+	}
+	defer toFile.Close()
+
+	fmt.Println(limit)
+	_, err = io.CopyN(toFile, fromFile, limit)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			err = nil
+			return nil
+		}
+		return err
+	}
+
 	return nil
+}
+
+func CompareFiles(expected, actual string) (bool, error) {
+	bytes1, err := os.ReadFile(expected)
+	if err != nil {
+		return false, ErrReadFile
+	}
+
+	bytes2, err := os.ReadFile(actual)
+	if err != nil {
+		return false, ErrReadFile
+	}
+
+	return bytes.Equal(bytes1, bytes2), nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,55 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	originalFilePath := "testdata/input.txt"
+	resultFilePath := "output.txt"
+
+	t.Run("test offset0 limit0", func(t *testing.T) {
+		expectedFilePath := "testdata/out_offset0_limit0.txt"
+
+		limit := int64(0)
+		offset := int64(0)
+
+		err := Copy(originalFilePath, resultFilePath, limit, offset)
+		if err != nil {
+
+		}
+
+		result, err := CompareFiles(expectedFilePath, resultFilePath)
+		if err != nil {
+
+		}
+
+		assert.True(t, result)
+		err = os.Remove(resultFilePath)
+	})
+
+	t.Run("test offset0 limit10", func(t *testing.T) {
+		expectedFilePath := "testdata/out_offset0_limit10.txt"
+
+		offset := int64(0)
+		// TODO - это прикол винды? С тем что перенос строки занимает 2 байта?
+		limit := int64(11)
+
+		err := Copy(originalFilePath, resultFilePath, offset, limit)
+		if err != nil {
+
+		}
+
+		result, err := CompareFiles(expectedFilePath, resultFilePath)
+		if err != nil {
+
+		}
+
+		assert.True(t, result)
+		err = os.Remove(resultFilePath)
+	})
+
+	// TODO ошибки
 }
