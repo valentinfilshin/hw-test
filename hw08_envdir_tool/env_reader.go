c8a341a5a297d16aac0e88aebe266d1db89b6bb4
Valentin Filshin <valentinfilshin@gmail.com>
2025-05-17 00:21:24 +0300

    HW08: Envdir tool - сделал env_reader и тесты к нему

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +19,67 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	fileSlice, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(fileSlice)
+
+	environmentMap := make(Environment, len(dir))
+
+	for _, file := range fileSlice {
+		fileName := file.Name()
+
+		if file.IsDir() || strings.Contains(fileName, "=") {
+			continue
+		}
+
+		if envValue, err := readEnvFile(dir, fileName); err == nil {
+			environmentMap[fileName] = envValue
+		}
+	}
+
+	return environmentMap, nil
+}
+
+func readEnvFile(dir, fileName string) (EnvValue, error) {
+	filePath := path.Join(dir, fileName)
+
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return EnvValue{}, err
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return EnvValue{}, fmt.Errorf("%s is not a regular file", fileName)
+	}
+
+	fileSize := fileInfo.Size()
+
+	if fileSize == 0 {
+		return EnvValue{"", true}, nil
+	}
+
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return EnvValue{}, err
+	}
+
+	str := formatFileData(fileData)
+
+	return EnvValue{str, false}, nil
+}
+
+func formatFileData(fileData []byte) string {
+	idx := bytes.Index(fileData, []byte("\n"))
+
+	if idx != -1 {
+		fileData = fileData[:idx]
+	}
+
+	fileData = bytes.ReplaceAll(fileData, []byte("\000"), []byte("\n"))
+	fileData = bytes.TrimRight(fileData, " \t")
+
+	return string(fileData)
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,36 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	expectedEnv := Environment{
+		"BAR":    {Value: "bar", NeedRemove: false},
+		"BARUHA": {Value: "baruha u uha!", NeedRemove: false},
+		"EMPTY":  {NeedRemove: false},
+		"FOO":    {Value: "   foo\nwith new line", NeedRemove: false},
+		"HELLO":  {Value: "\"hello\"", NeedRemove: false},
+		"UNSET":  {NeedRemove: true},
+	}
+
+	t.Run("check test cases", func(t *testing.T) {
+		testDir := "testdata/env"
+		testDirEnv, err := ReadDir(testDir)
+
+		require.Nil(t, err)
+		require.Equal(t, expectedEnv, testDirEnv)
+	})
+
+	t.Run("no directory", func(t *testing.T) {
+		_, err := ReadDir("test")
+		require.NotNil(t, err)
+	})
+
+	t.Run("wrong directory", func(t *testing.T) {
+		_, err := ReadDir("/dev/random")
+		require.NotNil(t, err)
+	})
 }
