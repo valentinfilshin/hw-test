cfcc0df1ec2fa1e83d1081fb36322bba242199c7
Valentin Filshin <filshin.va@nikamed-it.ru>
2025-05-25 22:43:26 +0300

    Init commit - questions, config, logger

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -8,16 +8,17 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
+	"github.com/valentinfilshin/hw-test/hw12_13_14_15_calendar/internal/app"
+	"github.com/valentinfilshin/hw-test/hw12_13_14_15_calendar/internal/config"
+	"github.com/valentinfilshin/hw-test/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/valentinfilshin/hw-test/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/valentinfilshin/hw-test/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 var configFile string
 
 func init() {
-	flag.StringVar(&configFile, "config", "/etc/calendar/config.toml", "Path to configuration file")
+	flag.StringVar(&configFile, "config", "configs/config.yaml", "Path to configuration file")
 }
 
 func main() {
@@ -28,8 +29,8 @@ func main() {
 		return
 	}
 
-	config := NewConfig()
-	logg := logger.New(config.Logger.Level)
+	cfg := config.NewConfig(configFile)
+	logg := logger.New(cfg.Logger)
 
 	storage := memorystorage.New()
 	calendar := app.New(logg, storage)
@@ -52,6 +53,7 @@ func main() {
 	}()
 
 	logg.Info("calendar is running...")
+	logg.Debug("calendar is running...")
 
 	if err := server.Start(ctx); err != nil {
 		logg.Error("failed to start http server: " + err.Error())
--- hw12_13_14_15_16_calendar/go.mod
+++ hw12_13_14_15_16_calendar/go.mod
@@ -1,3 +1,11 @@
-module github.com/fixme_my_friend/hw12_13_14_15_calendar
+module github.com/valentinfilshin/hw-test/hw12_13_14_15_calendar
 
 go 1.23
+
+require github.com/heetch/confita v0.10.0
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
