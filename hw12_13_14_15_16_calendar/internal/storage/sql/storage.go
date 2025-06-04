e0dfd50e4612f6da83d06fe1995d51eec4c0804a
Valentin Filshin <filshin.va@nikamed-it.ru>
2025-06-04 13:45:07 +0300

    TODO

--- hw12_13_14_15_16_calendar/internal/storage/sql/storage.go
+++ hw12_13_14_15_16_calendar/internal/storage/sql/storage.go
@@ -41,7 +41,10 @@ func (s *Storage) Close(ctx context.Context) error {
 	return nil
 }
 
+// TODO что будет если одновременно два запроса попробуют забронировать свободное пересекающееся время
+
 func (s *Storage) AddEvent(event storage.Event) error {
+	// Проверка на существование других с пересекающимся временем перед вставкой + transaction?
 	_, err := s.db.Exec(
 		"INSERT INTO events (id, title, start_time, end_time, description, user_id, notify_before) VALUES ($1, $2, $3, $4, $5, $6, $7)",
 		event.ID, event.Title, event.StartTime, event.EndTime, event.Description, event.UserID, event.NotifyBefore)
@@ -53,6 +56,7 @@ func (s *Storage) AddEvent(event storage.Event) error {
 }
 
 func (s *Storage) ChangeEvent(event storage.Event) error {
+	// Проверка на существование других с пересекающимся временем перед обновлением + transaction?
 	_, err := s.db.Exec(
 		"UPDATE events SET title = $1, start_time = $2, end_time = $3, description = $4, user_id = $5, notify_before = $6 WHERE id = $7",
 		event.Title, event.StartTime, event.EndTime, event.Description, event.UserID, event.NotifyBefore, event.ID)
@@ -73,7 +77,8 @@ func (s *Storage) RemoveEvent(id string) error {
 }
 
 func (s *Storage) GetEvents(userID int, from, to time.Time) ([]storage.Event, error) {
-	result, err := s.db.Exec("SELECT * FROM events WHERE user_id = $1 AND start_time >= $2 AND end_time <= $3", userID, from, to)
+	// TODO маппинг в структуры
+	result, err := s.db.Exec("SELECT * FROM events WHERE user_id = $1 AND (start_time >= $2 OR end_time <= $3)", userID, from, to)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get events: %w", err)
 	}
